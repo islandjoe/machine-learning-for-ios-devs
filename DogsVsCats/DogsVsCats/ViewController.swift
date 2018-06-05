51d02568762f66716aeaf56de1acc785c7e491d7
Arthur Kho Caayon <islandjoe@gmail.com>
2018-06-05 11:54:03 +0300

    Dogs vs Cats iOS app

---
+++ DogsVsCats/DogsVsCats/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var checkAnimalButton: UIButton!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var classificationLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func selectImageSource(_ sender: Any) {
+    
+  }
+  
+  @IBAction func checkAnimal(_ sender: Any) {
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
